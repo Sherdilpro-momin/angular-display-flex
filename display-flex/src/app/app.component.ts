5fc75fa8952392fe5aad4bfcac987d0149f1993f
Muhammad Bilal <muhammad.bilal@mominsolutions.net>
2023-08-01 14:31:28 +0500

    implemented ag-grid and tested required scenario

--- display-flex/src/app/app.component.ts
+++ display-flex/src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import { AgGridAngular } from 'ag-grid-angular';
+import { CellClickedEvent, ColDef, GridReadyEvent } from 'ag-grid-community';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -32,56 +35,108 @@ export class AppComponent {
       "age":24,
       "weight":60,
       "fullName":"test adam",
-      "dob":"march 24th, 1998"
+      "dob":"march 24th, 1998",
+      "country":"Pakistan",
+      "city":"Lahore"
     },
     {
       "name":"test",
       "age":24,
       "weight":60,
       "fullName":"test adam",
-      "dob":"march 24th, 1998"
+      "dob":"march 24th, 1998",
+      "country":"Pakistan",
+      "city":"Lahore"
     },
     {
       "name":"test",
       "age":24,
       "weight":60,
       "fullName":"test adam",
-      "dob":"march 24th, 1998"
+      "dob":"march 24th, 1998",
+      "country":"Pakistan",
+      "city":"Lahore"
     },
     {
       "name":"test",
       "age":24,
       "weight":60,
       "fullName":"test adam",
-      "dob":"march 24th, 1998"
+      "dob":"march 24th, 1998",
+      "country":"Pakistan",
+      "city":"Lahore"
     },
     {
       "name":"test",
       "age":24,
       "weight":60,
       "fullName":"test adam",
-      "dob":"march 24th, 1998"
+      "dob":"march 24th, 1998",
+      "country":"Pakistan",
+      "city":"Lahore"
     },
     {
       "name":"test",
       "age":24,
       "weight":60,
       "fullName":"test adam",
-      "dob":"march 24th, 1998"
+      "dob":"march 24th, 1998",
+      "country":"Pakistan",
+      "city":"Lahore"
     },
     {
       "name":"test",
       "age":24,
       "weight":60,
       "fullName":"test adam",
-      "dob":"march 24th, 1998"
+      "dob":"march 24th, 1998",
+      "country":"Pakistan",
+      "city":"Lahore"
     },
     {
       "name":"test",
       "age":24,
       "weight":60,
       "fullName":"test adam",
-      "dob":"march 24th, 1998"
+      "dob":"march 24th, 1998",
+      "country":"Pakistan",
+      "city":"Lahore"
     }
   ]
+  // Each Column Definition results in one Column.
+  public columnDefs: ColDef[] = [
+    { field: 'make'},
+    { field: 'model'},
+    { field: 'price' }
+  ];
+
+  // DefaultColDef sets props common to all Columns
+  public defaultColDef: ColDef = {
+    sortable: true,
+    filter: true,
+  };
+  
+  // Data that gets displayed in the grid
+  public rowData$!: Observable<any[]>;
+
+  // For accessing the Grid's API
+  @ViewChild(AgGridAngular) agGrid!: AgGridAngular;
+
+  constructor(private http: HttpClient) {}
+
+  // Example load data from server
+  onGridReady(params: GridReadyEvent) {
+    this.rowData$ = this.http
+      .get<any[]>('https://www.ag-grid.com/example-assets/row-data.json');
+  }
+
+  // Example of consuming Grid Event
+  onCellClicked( e: CellClickedEvent): void {
+    console.log('cellClicked', e);
+  }
+
+  // Example using Grid's API
+  clearSelection(): void {
+    this.agGrid.api.deselectAll();
+  }
 }
